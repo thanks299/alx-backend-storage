8f20470055e1ffe19a79ed74b7096f0df819d6b1
thanks299 <thanksagbeble@gmail.com>
2024-08-07 18:16:20 -0700

    sql advance task

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- Script to create a table `users` with the specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
