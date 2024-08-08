2b088e4ed3c6fe59254a6c3c0879ff19fe43154e
thanks299 <thanksagbeble@gmail.com>
2024-08-08 07:55:33 -0700

    sql advance task

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DROP TRIGGER IF EXISTS decrease_quantity;
+DELIMITER $$
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
