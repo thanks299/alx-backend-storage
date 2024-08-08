b837dd02b7737e9bd7578e87e79ffc1608bfe751
thanks299 <thanksagbeble@gmail.com>
2024-08-07 18:41:45 -0700

    sql advance task

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Script to rank country origins of bands by the number of fans
-SELECT origin, SUM(nb_fans) as nb_fans
+SELECT origin, SUM(fans) AS nb_fans
   FROM metal_bands
   GROUP BY origin
   ORDER BY nb_fans DESC;
